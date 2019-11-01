b9cf683e37712db0c737386c715836c66ff64a89
AnthonyTB <Anthonylol@protonmail.com>
2019-11-01 09:05:38 -0700

    added functions

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+// handles the input of how many dog images to log into console
 
+function handleDogImageAmount() {
+  $('#dataEntry').on('submit', e => {
+    console.log('button works');
+    // e.preventDefault();
+
+    // let amount = e.currentTarget.value;
+  });
+}
+
+function displayResults(input) {
+  console.log(input);
+}
+
+function apiCall(amount) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${amount}`)
+    .then(response => response.json())
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => alert('Error'));
+}
